ab280df3d255925d08cae7727fdf0a5a753aadb2
Meng Ma <meng.ma@bytedance.com>
2022-02-23 18:35:47 -0800

    [WIP] Using RefVariable

--- test_zero_out_list_mutable.py
+++ test_zero_out_list_mutable.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.python.ops import variables
 
 tf.compat.v1.disable_eager_execution()
 
@@ -8,8 +9,8 @@ tf.compat.v1.disable_eager_execution()
 # b = [3,4]
 # print(zero_out_module.zero_out(a))
 
-a = tf.Variable([1,2], dtype=np.int32, name="a", trainable=False)
-print(a.dtype._is_ref_dtype)
+# a = tf.Variable([1,2], dtype=np.int32, name="a", trainable=False)
+# print(a.dtype._is_ref_dtype)
 # x = tf.compat.v1.get_variable('x', shape=(2,), dtype=tf.float32)
 # print(x.dtype)
 # b = tf.Variable(np.array([3,4,5], dtype=np.int32), name="b", trainable=False)
@@ -18,15 +19,19 @@ print(a.dtype._is_ref_dtype)
 
 zero_out_list_mutable_module = tf.load_op_library('./zero_out_list_mutable.so')
 
-with tf.compat.v1.Session() as sess:
-	tf.compat.v1.global_variables_initializer().run()
-	update = a.assign([2,3])
-	sess.run(update)
-	print("after assignment, a =", a.eval())
-	res = zero_out_list_mutable_module.zero_out_list_mutable(a)
-	print(sess.run([res]))
+with tf.Graph().as_default():
+	with tf.compat.v1.Session() as sess:
+		a = variables.RefVariable([1,2], dtype=np.int32, name="a", trainable=False)
+		print(a.dtype._is_ref_dtype)
 
-print(f"{a=} {b=} {c=} {d=}")
+		tf.compat.v1.global_variables_initializer().run()
+		update = a.assign([2,3])
+		sess.run(update)
+		print("after assignment, a =", a.eval())
+		res = zero_out_list_mutable_module.zero_out_list_mutable(a, [3,4])
+		print(sess.run([res]))
+
+# print(f"{a=} {b=} {c=} {d=}")
 
 # Prints
 # array([[1, 0], [0, 0]], dtype=int32)
--- zero_out_list_mutable.cc
+++ zero_out_list_mutable.cc
@@ -1,24 +1,48 @@
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/shape_inference.h"
 
+#include "tensorflow/core/framework/common_shape_fns.h"
+
 #include "tensorflow/core/framework/op_kernel.h"
 
 #include <iostream>
 
 using namespace tensorflow;
 
-REGISTER_OP("ZeroOutListMutable")
-	// .Attr("N: int = 2")
-	// .Attr("T: int32")
-	.Input("to_zero: Ref(int32)")
-    // .Input("to_zero: Ref(N * int32)")
-    // .Input("to_zero1: Ref(N * int32)")
-    // .Output("zeroed: int32")
-    // .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-    //   c->set_output(0, c->input(0));
-    //   return Status::OK();
-    // })
-    ;
+using shape_inference::InferenceContext;
+using shape_inference::ShapeHandle;
+
+// REGISTER_OP("ZeroOutListMutable")
+// 	// .Attr("N: int = 2")
+// 	// .Attr("T: int32")
+// 	.Input("to_zero: Ref(int32)")
+//     // .Input("to_zero: Ref(N * int32)")
+//     // .Input("to_zero1: Ref(N * int32)")
+//     .Output("zeroed: int32");
+//     // .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+//     //   c->set_output(0, c->input(0));
+//     //   return Status::OK();
+//     // })
+//     // ;
+
+ REGISTER_OP("ZeroOutListMutable")
+    .Input("ref: Ref(T)")
+    .Input("value: T")
+    .Output("output_ref: Ref(T)")
+    .Attr("T: type")
+    .Attr("validate_shape: bool = true")
+    .Attr("use_locking: bool = true")
+    .SetAllowsUninitializedInput()
+    .SetShapeFn([](InferenceContext* c) {
+      bool validate_shape;
+      TF_RETURN_IF_ERROR(c->GetAttr("validate_shape", &validate_shape));
+      if (validate_shape) {
+        return shape_inference::MergeBothInputsShapeFn(c);
+      }
+
+      c->set_output(0, c->input(1));
+      return Status::OK();
+    });
 
 
 class ZeroOutListMutableOp : public OpKernel {
@@ -28,8 +52,8 @@ private:
 public:
 	explicit ZeroOutListMutableOp(OpKernelConstruction* context) : OpKernel(context) {
 		// std::cout << "Total number of input is " << context->num_inputs() << "\n";
-		context->GetAttr("N", &N_);
-		std::cout << "get value of N " << N_ << '\n';
+		// context->GetAttr("N", &N_);
+		// std::cout << "get value of N " << N_ << '\n';
 	}
 
 	void Compute(OpKernelContext* context) override {
@@ -43,18 +67,18 @@ public:
 		// }
 
 		// Test inputlist
-		OpMutableInputList list1;
-		context->mutable_input_list("to_zero", &list1);
-		std::cout << "list of argument 1 size " << list1.size() << '\n';
-		std::cout << "The shape of tensors in list1 is : ";
-		for(int i = 0; i < list1.size(); ++i) {
-			std::cout << list1.at(i, false).shape() << " ";
-			auto t = list1.at(i, false);
-			auto tf = t.flat<int32>();
-			tf(0) = 0;
-			std::cout << "set t[0] = 0\n";
-		}
-		std::cout << " that's for tensors in list1\n";
+		// OpMutableInputList list1;
+		// context->mutable_input_list("ref", &list1);
+		// std::cout << "list of argument 1 size " << list1.size() << '\n';
+		// std::cout << "The shape of tensors in list1 is : ";
+		// for(int i = 0; i < list1.size(); ++i) {
+		// 	std::cout << list1.at(i, false).shape() << " ";
+		// 	auto t = list1.at(i, false);
+		// 	auto tf = t.flat<int32>();
+		// 	tf(0) = 0;
+		// 	std::cout << "set t[0] = 0\n";
+		// }
+		// std::cout << " that's for tensors in list1\n";
 
 		// OpInputList list2;
 		// context->input_list("to_zero1", &list2);
@@ -70,11 +94,11 @@ public:
 		// auto input = input_tensor.flat<int32>();
 
 		// Create an output tensor
-		// Tensor* output_tensor = NULL;
-		// OP_REQUIRES_OK(context, context->allocate_output(0, TensorShape(),
-		//                                                  &output_tensor));
-		// auto output_flat = output_tensor->flat<int32>();
-		// output_flat(0) = 155;
+		Tensor* output_tensor = NULL;
+		OP_REQUIRES_OK(context, context->allocate_output(0, context->input(0).shape(),
+		                                                 &output_tensor));
+		auto output_flat = output_tensor->flat<int32>();
+		output_flat(0) = 155;
 
 		// // Set all but the first element of the output tensor to 0.
 		// const int N = input.size();
